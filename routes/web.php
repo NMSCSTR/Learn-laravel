eb02bc044221f91966e285e21d255b9bb1f0c8c9
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-02-16 13:26:46 +0800

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note', [NoteController::class, 'create'])->name('note.create');
